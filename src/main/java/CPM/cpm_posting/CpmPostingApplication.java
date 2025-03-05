c79e82dab2559a34d4884abfd7ae302eb599dfab
Muhammad Iqbal <iqbalsetiawan536@gmail.com>
2025-03-05 08:17:26 +0700

    init

---
+++ src/main/java/CPM/cpm_posting/CpmPostingApplication.java
@@ -0,0 +1,13 @@
+package CPM.cpm_posting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CPM/cpm_posting/CpmPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package CPM.cpm_posting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
